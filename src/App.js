2a536f7bfa2b0522fb4971e4c5c92d9248201198
Arthur Alves <arthuralves91@hotmail.com>
2020-07-02 21:08:24 -0300

    insere, lista e deleta projetos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories')
+    .then(response => setRepositories(response.data)
+  )}, []); 
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+      title: `New Project ${(Math.random() * 10)}`,
+      url: 'https://github.com/arthuralveso/',
+      techs: ['React.js', 'Node.js']
+    });
+
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id))
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repositorie => 
+          <li key={repositorie.id}> 
+          {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
         </li>
+      )}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
